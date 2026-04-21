0191e22ec572a17138408289debbd4f72e1cc587
Axel Fernando Mora Mantilla <axfermm10@gmail.com>
2026-04-20 21:19:56 -0600

    Update ip_scanner.py

--- ip_scanner.py
+++ ip_scanner.py
@@ -2,7 +2,6 @@ import subprocess
 from multiprocessing import Pool
 import ipaddress
 import platform
-import os
 import time
 
 # ----------------------------------------------------------------------------
@@ -14,17 +13,24 @@ def permission(ip):
             ["ping", "-c", "1", "-W", "1", ip],
             stdout=subprocess.DEVNULL
         )
-
-    if operating_sys == 'Windows':
-        answer = os.system('ping %s -n 1' % (ip,))
-
-    return answer.returncode == 0
+        return answer.returncode == 0
+    elif operating_sys == 'Windows':
+        host = ip
+        command = ["ping", "-n", "1", host]
+        answer = subprocess.run(command, capture_output=True, text=True)
+        return answer.returncode == 0
+    else:
+        return False
 
 # ----------------------------------------------------------------------------
 
 def ip_scan(result):
+    answer_time = time.time()
     status_quo = permission(result)
-    return (result,status_quo)
+    answer_time_end = time.time()
+    answer_time_total = answer_time_end - answer_time
+    answer_time_total = round(answer_time_total, 4)
+    return (result,status_quo, answer_time_total)
 
 print("Example in a IP address: 100.10.10")
 print("Ranges in an IP direction are from 1 to 254")
@@ -70,18 +76,18 @@ if __name__ == "__main__":
 
     with Pool(10) as p:
         results = p.map(ip_scan, ip_pool)
-        for result, status_quo in results:
-
+        for result, status_quo, answer_time_total in results:
             if status_quo:
                 print(f"Scanned IP {result} is responsive!")
+                print(f"Total time taken: {answer_time_total} seconds.")
 
-endtime = time.time()
-print(f"Total time taken: {endtime - start_time} seconds.")
+    endtime = time.time()
+    print(f"Total time taken: {endtime - start_time} seconds.")
+
 
 # ----------------------------------------------------------------------------
 
-# Upgrades: operating system detection (platform.system()).
-# Add timing
+# Upgrades:
 # Detect mask automatically (low priority)
 # sub red full scan with ipaddress.ip_network()
 # Usage of "socket" library just to create a tool more precise
